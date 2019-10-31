3c8e00d9d176744acc04b6a1e3e0600cea255ac2
Alvin Putra Syarif Laoli <55613473+alvinputrasyariflaoli@users.noreply.github.com>
2019-10-31 11:19:35 +0700

    Add files via upload

---
+++ program_1914370109_1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+ int UAS,UTS;
+ float hasil;
+ printf("Nilai UAS :");
+  scanf("%d", &UAS);
+ printf("Nilai UTS :");
+ scanf("%d", &UTS);
+ if (UAS>=70){
+        printf("anda lulus");
+    }else{
+        if (hasil>60){
+        printf("tidak lulus");
+        }else{
+        printf("tidak lulus");
+        }
+            }
+
+    return 0;
+}
+
